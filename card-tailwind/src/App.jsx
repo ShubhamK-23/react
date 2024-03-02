9543ea851b0e159aa07078e8cbd57336561dd36c
ShubhamK-23 <sdkhatavkar22@gmail.com>
2024-03-03 05:16:50 +0530

    Tailwind Card

---
+++ card-tailwind/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <h1 >Tailwind-Card</h1> <br></br>
+      <Card systemName= 'MAC Book'/> <br></br>
+      <Card />
+      
+    </>
+  )
+}
+
+export default App
