f6faa66f73aac99754ca3075fa92af5788e0239d
ShubhamK-23 <sdkhatavkar22@gmail.com>
2024-03-10 12:49:39 +0530

    ThemeChanger using context API

--- Theme Changer/src/App.jsx
+++ Theme Changer/src/App.jsx
@@ -1,13 +1,42 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import Card from './components/Card/Card'
+import Themebtn from './components/Themebtn/Themebtn'
+import { ThemeProvider } from './context/Theme'
 
 function App() {
+  const [themeMode, setThemeMode] = useState('light')
 
+  const lightTheme= () => {
+    setThemeMode('light')
+  }
 
-  return (
-    <>
-      <h1 className= 'bg-pink-500 p-4'>Shubham</h1>
-    </>
-  )
-}
+  const darkTheme  = () => {
+    setThemeMode('dark')
+  }
+
+  useEffect( () => {
+    document.querySelector('html').classList.remove('light', 'dark')
+    document.querySelector('html').classList.add(themeMode)
+  })
+
+    return (
+      <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+        <div className="flex flex-wrap min-h-screen items-center">
+            <div className="w-full">
+                <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                    <Themebtn />
+                </div>
+  
+                <div className="w-full max-w-sm mx-auto">
+                    <Card />
+                </div>
+            </div>
+        </div>
+      </ThemeProvider>
+    )
+  }
+  
+
 
 export default App
