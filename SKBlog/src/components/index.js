65bb78ea8f0fe0a3401e289c2e0aa86b48462238
ShubhamK-23 <sdkhatavkar22@gmail.com>
2024-03-27 20:46:48 +0530

    Blog website working

---
+++ SKBlog/src/components/index.js
@@ -0,0 +1,16 @@
+import Footer from './Footer/Footer'
+import Header from './Header/Header.jsx'
+import Container from "./container/Container"
+import Logo from './Logo'
+import LogoutBtn from './Header/LogoutBtn.jsx'
+import Button from './Button.jsx'
+import Input from './Input.jsx'
+import Select from './Select.jsx'
+import RTE from './rte/RTE.jsx'
+import Signup from './signup/Signup.jsx'
+import  Login  from "./login/Login.jsx";
+import PostCard from "../PostCard.jsx"
+import PostForm from './post-form/PostForm.jsx'
+import AuthLayout from './AuthLayout.jsx'
+
+export {Footer,Container, Logo, Header, LogoutBtn, Input, Select, Button, RTE, Signup, Login,PostCard, PostForm, AuthLayout }
